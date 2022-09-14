d9815f9e36ed32aaba65d3e24ff6c3fd892a0d72
Luis Felipe <luisfelipe@MACBOOK-PRO-DE-ADESKTOP.local>
2022-09-14 15:43:18 -0300

    implementação do método transfere

--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -4,8 +4,23 @@ public class TestaMetodo {
         contaDoPipe.saldo = 100;
         contaDoPipe.deposita(50);
         System.out.println(contaDoPipe.saldo);
+
         boolean conseguiuRetirar = contaDoPipe.saca(20);
         System.out.println(contaDoPipe.saldo);
         System.out.println(conseguiuRetirar);
+
+        Conta contaDaMarcela = new Conta();
+        contaDaMarcela.deposita(1000);
+        boolean sucessoTransferencia = contaDaMarcela.transfere(300, contaDoPipe);
+
+        if(sucessoTransferencia) {
+            System.out.println("transferencia realizada com sucesso");
+        }
+        else {
+            System.out.println("saldo insuficiente");
+        }
+        System.out.println(contaDaMarcela.saldo);
+        System.out.println(contaDoPipe.saldo);
+
     }
 }
